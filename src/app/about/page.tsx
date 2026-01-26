b041876a5f2caf4cb0cd78e42b6b69da0a4c58c8
Caio De Blasio <caio.blasio@n26.com>
2026-01-26 20:36:09 +0100

    Adds blog and about routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Blog() {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold">About</h1>
+      <Link href="/">Back to Home</Link>
+    </div>
+  );
+}
